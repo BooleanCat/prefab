f5ec21ee4bfa2d89101ec0cef0a1f8d4d47d5f07
Tom Godkin <thgodkin@gmail.com>
2018-09-09 15:25:25 +0100

    implement -v and -h

--- spec/prefab_spec.lua
+++ spec/prefab_spec.lua
@@ -1,14 +1,53 @@
+when = describe
+
 describe('prefab', function()
-	it('does nothing', function()
-		local output = execute('./target/release/prefab', 'r')
-		assert.are.equal(output, 'Hello, world!\n')
+	when('invoked with -h', function()
+		local state
+
+		before_each(function()
+			state = execute('./target/release/prefab -h', 'r')
+		end)
+
+		it('succeeds', function()
+			assert.is_true(state.success)
+		end)
+
+		it('prints help', function()
+			assert.is_not_nil(string.find(state.output, "USAGE"))
+		end)
+	end)
+
+	when('invoked with -V', function()
+		local state
+
+		before_each(function()
+			state = execute('./target/release/prefab -V', 'r')
+		end)
+
+		it('succeeds', function()
+			assert.is_true(state.success)
+		end)
+
+		it('prints the version', function()
+			assert.are.equal("dev\n", state.output)
+		end)
+	end)
+
+	when('invoked incorrectly', function()
+		it('fails', function()
+			local state = execute('./target/release/prefab', 'r')
+			assert.is_false(state.success)
+		end)
 	end)
 end)
 
 function execute(name)
 	local handle = io.popen(name, 'r')
 	local output = handle:read('*a')
-	local success, _ = handle:close()
-	assert.is_true(success)
-	return output
+	local exitstatus = {handle:close()}
+	return {
+		output=output,
+		success=exitstatus[1] == true,
+		exitcode=exitstatus[3],
+	}
 end
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    #[structopt(short = "V", long = "version")]
+    version: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    if opt.version {
+        println!("dev");
+        std::process::exit(0);
+    }
+    std::process::exit(1);
 }
