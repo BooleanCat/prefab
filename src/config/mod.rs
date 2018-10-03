d833fc8619969fbfcbd8a1dc59ea2fe2e1a4b244
Tom Godkin <thgodkin@gmail.com>
2018-10-03 08:12:02 +0100

    fill some missing tests

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -26,23 +26,73 @@ mod tests {
     use serde_json;
 
     #[test]
-    fn serialize_deserialize_config() {
-        let root = Root{path: String::from("/foo/bar"), readonly: true};
-        let process = Process{
-            terminal: true,
-            console_size: ConsoleSize{height: 1, width: 1},
-            cwd: String::from("/bar/baz"),
-            env: vec![String::from("FOO=BAR")],
-        };
-        let config = Config{
+    fn serialize_config() {
+        let json: serde_json::Value = serde_json::from_str(&serde_json::to_string(&config_prototype()).unwrap()).unwrap();
+
+        let expected = json!({
+            "ociVersion": "foo",
+            "root": {
+                "path": "/foo/bar",
+                "readonly": true
+            },
+            "process": {
+                "terminal": true,
+                "consoleSize": {
+                    "height": 100,
+                    "width": 200
+                },
+                "cwd": "/foo/bar",
+                "env": ["FOO=BAR"]
+            }
+        });
+
+        assert_eq!(expected, json);
+    }
+
+    #[test]
+    fn deserialize_config() {
+        let json = r#"{
+            "ociVersion": "foo",
+            "root": {
+                "path": "/foo/bar",
+                "readonly": true
+            },
+            "process": {
+                "terminal": true,
+                "consoleSize": {
+                    "height": 100,
+                    "width": 200
+                },
+                "cwd": "/foo/bar",
+                "env": ["FOO=BAR"]
+            }
+        }"#;
+
+        let config: Config = serde_json::from_str(json).unwrap();
+
+        assert_eq!(config_prototype(), config);
+    }
+
+    #[test]
+    fn deserialize_config_optional_fields() {
+        let config: Config = serde_json::from_str(r#"{
+            "ociVersion": "foo",
+            "root": {
+                "path": "/foo/bar",
+                "readonly": true
+            }
+        }"#).unwrap();
+
+        let expected = Config{
             oci_version: String::from("foo"),
-            root: root,
-            process: process,
+            root: Root{
+                path: String::from("/foo/bar"),
+                readonly: true,
+            },
+            ..Default::default()
         };
 
-        let deserialized_config: Config = serde_json::from_str(&serde_json::to_string(&config).unwrap()).unwrap();
-
-        assert_eq!(deserialized_config, config);
+        assert_eq!(expected, config);
     }
 
     #[test]
@@ -56,16 +106,22 @@ mod tests {
         assert_eq!(expected, root);
     }
 
-    #[test]
-    fn deserialize_config_process_optional() {
-        let config: Config = serde_json::from_str(r#"{
-            "ociVersion": "",
-            "root":{
-                "path": ""
-            }
-        }"#).unwrap();
-        let expected = Config{process: Default::default(), ..Default::default()};
-
-        assert_eq!(expected, config);
+    fn config_prototype() -> Config {
+        Config{
+            oci_version: String::from("foo"),
+            root: Root {
+                path: String::from("/foo/bar"),
+                readonly: true,
+            },
+            process: Process{
+                terminal: true,
+                console_size: ConsoleSize{
+                    height: 100,
+                    width: 200,
+                },
+                cwd: String::from("/foo/bar"),
+                env: vec![String::from("FOO=BAR")],
+            },
+        }
     }
 }
