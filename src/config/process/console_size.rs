9ae049fdff5194e4a5b60e1995d8fe4405cd0fe1
Tom Godkin <thgodkin@gmail.com>
2018-10-03 12:58:00 +0100

    Move ConsoleSize to a submodule
    Co-authored-by: Giuseppe Capizzi <g.capizzi@gmail.com>

---
+++ src/config/process/console_size.rs
@@ -0,0 +1,36 @@
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
+pub struct ConsoleSize {
+    pub height: usize,
+    pub width: usize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ConsoleSize;
+    use serde_json;
+
+    #[test]
+    fn serialize_console_size() {
+        let json: serde_json::Value = serde_json::from_str(&serde_json::to_string(&ConsoleSize{height: 100, width: 200}).unwrap()).unwrap();
+
+        let expected = json!({
+            "height": 100,
+            "width": 200
+        });
+
+        assert_eq!(expected, json);
+    }
+
+    #[test]
+    fn deserialize_console_size() {
+        let json = r#"{
+            "height": 100,
+            "width": 200
+        }"#;
+
+        let console_size: ConsoleSize = serde_json::from_str(json).unwrap();
+        let expected = ConsoleSize{height: 100, width: 200};
+
+        assert_eq!(expected, console_size);
+    }
+}
--- src/config/process/mod.rs
+++ src/config/process/mod.rs
@@ -1,3 +1,7 @@
+mod console_size;
+
+pub use self::console_size::ConsoleSize;
+
 #[serde(rename_all = "camelCase")]
 #[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Process {
@@ -13,15 +17,9 @@ pub struct Process {
     pub env: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
-pub struct ConsoleSize {
-    pub height: usize,
-    pub width: usize,
-}
-
 #[cfg(test)]
 mod tests {
-    use super::{Process, ConsoleSize};
+    use super::Process;
     use serde_json;
 
     #[test]
@@ -72,31 +70,6 @@ mod tests {
         };
 
         assert_eq!(expected, process);
-    }
-
-    #[test]
-    fn serialize_console_size() {
-        let json: serde_json::Value = serde_json::from_str(&serde_json::to_string(&ConsoleSize{height: 100, width: 200}).unwrap()).unwrap();
-
-        let expected = json!({
-            "height": 100,
-            "width": 200
-        });
-
-        assert_eq!(expected, json);
-    }
-
-    #[test]
-    fn deserialize_console_size() {
-        let json = r#"{
-            "height": 100,
-            "width": 200
-        }"#;
-
-        let console_size: ConsoleSize = serde_json::from_str(json).unwrap();
-        let expected = ConsoleSize{height: 100, width: 200};
-
-        assert_eq!(expected, console_size);
     }
 
     fn process_prototype() -> Process {
